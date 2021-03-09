3bd5bee864fb0f8d5e6dd7a9af33fd604bf8ca25
TTBoiMike <47759631+TTBoiMike@users.noreply.github.com>
2021-03-09 13:03:54 +0000

    Update OurQuality.js

--- src/components/About/OurQuality.js
+++ src/components/About/OurQuality.js
@@ -16,7 +16,7 @@ let OurQuality = () => {
                     Although we work with growers who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast
                     consistent, user-friendly coffee, so that brewing is easy and enjoyable.
                 </p>
-                <Button text="Create a Plan" link="/subscription" />
+                <Button text="Create a Plan" link="/coffee-roaster-website/subscription" />
             </div>
         </section>
     )
