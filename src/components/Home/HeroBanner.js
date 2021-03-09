e95d4018315329d5d13c26a9bc2d867d5c61f93a
TTBoiMike <47759631+TTBoiMike@users.noreply.github.com>
2021-03-09 13:03:25 +0000

    Update HeroBanner.js

--- src/components/Home/HeroBanner.js
+++ src/components/Home/HeroBanner.js
@@ -11,7 +11,7 @@ let HeroBanner = () => {
                     Start your morning with the world's best coffees. Our expertly curated
                     artisan coffees are delivered directly to your day at a time that suits you.
                 </p>
-                <Button text="Create Plan" link="/subscription"/>
+                <Button text="Create Plan" link="/coffee-roaster-website/subscription"/>
             </div>
         </section>
     )
